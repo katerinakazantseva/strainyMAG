d6b6062bf813c7a7ccc6a57ca42c20875632a55b
Ekaterina Kazantseva <82141791+katerinakazantseva@users.noreply.github.com>
2023-08-23 16:32:15 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -8,17 +8,16 @@ def cov_filtered(list):
         with open(list, 'r') as f:
                lines = [line.rstrip('\n') for line in f]
                for line in lines:
-                   if int(line.split(',')[1].split(".")[0])>10:
+                   if int(line.split(',')[1].split(".")[0])>30:
                        bin=line.split(',')[0]
                        res.append("output/transformed_bins/%s.fa" % bin)
     except: pass
-    print(res)
     return(res)
 
 def get_best():
         res=[]
         print("Filter by MAG quality")
-        test="""awk '$2>=80 && $3<=11 {{ print $1}}' output/qa_bins/quality_report.tsv """
+        test="""awk '$2>=80 && $3<=20 {{ print $1}}' output/qa_bins/quality_report.tsv """
         proc = subprocess.Popen(test,  shell=True, stdout=subprocess.PIPE)
         stdout_value = proc.communicate()[0]
         stdout=re.sub('\'','',re.sub('b', '',str(stdout_value),1))
@@ -26,6 +25,5 @@ def get_best():
                 if item!='':
                     res.append("output/bams/%s.bam" % item)
                     res.append("output/bams/%s.bam.bai" % item)
-        print(res)
         return res
-#cov_filtered("output/coverage_list.lst")
+
